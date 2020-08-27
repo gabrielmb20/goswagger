3ad705717e6005d9f69881aa408d01fb942fb814
Gabriel <gabriel.mb20@gmail.com>
2020-08-27 17:20:08 -0600

    pruebas

--- go/api_default.go
+++ go/api_default.go
@@ -103,12 +103,10 @@ func AuthorsAuthorIdGet(w http.ResponseWriter, r *http.Request) {
 func AuthorsAuthorIdPut(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	authorId := params["authorId"]
-	bookId := params["bookId"]
-	log.Printf("authorId: ", authorId)
-	log.Printf("bookId: ", bookId)
+	var originalProduct map[Author]interface{}
+	id := params["authorId"]
 	for i, author := range authors {
-		if author.AuthorId == authorId {
+		if author.AuthorId == id {
 			authors = append(authors[:i], authors[i+1:]...)
 			var updatedAuthor Author
 			json.NewDecoder(r.Body).Decode(&updatedAuthor)
