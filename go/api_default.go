b8d49c6f0597c0b12fbb3c8c8b542395840876c6
Gabriel <gabriel.mb20@gmail.com>
2020-08-27 13:36:05 -0600

    pruebas

--- go/api_default.go
+++ go/api_default.go
@@ -108,7 +108,7 @@ func AuthorsAuthorIdPut(w http.ResponseWriter, r *http.Request) {
         }
         authors = append(authors[:i], authors[i+1:]...)
 
-	oldAuthor := authors[id]
+	oldAuthor,_ := json.Marshal(authors[i])
 	log.Printf("TEST",oldAuthor)
 
 	len := r.ContentLength
